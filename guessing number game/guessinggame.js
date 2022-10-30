0f11eb0fcb00e98be3177e1ab74d09801b8f36ce
Tammy-Louise Castle <tammylouisecastle1@gmail.com>
2022-10-30 19:34:38 +0000

    removed user input and added a select tag instead

--- guessing number game/guessinggame.js
+++ guessing number game/guessinggame.js
@@ -1,14 +1,13 @@
 //generates a random numbeer between 1 and 100
 randomNumber = Math.floor(Math.random() * 101);
 
-var guessedNum = prompt('Please guess a number between 1 and 100!');
-var guessedNumInt = parseInt(guessedNum);
+var guessedNumInt = (numbers);
 
 var numOfGuesses = 0;
 
 document.getElementById('submitGuess').onclick = function(){
 
-var numOfGuesses = document.getElementById('guess').value;
+var guessedNumInt = document.getElementById('guess').value;
 if (guessedNumInt=== randomNumber){
     console.log(`You guessed correctly in ${numOfGuesses} guesses!`);
 } else if (guessedNumInt < randomNumber) {
