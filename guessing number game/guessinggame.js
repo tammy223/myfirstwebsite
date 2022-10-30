80141692adbad22ab1a85069a11f28b134c16780
Tammy-Louise Castle <tammylouisecastle1@gmail.com>
2022-10-30 18:18:38 +0000

    started basic html and javascript

---
+++ guessing number game/guessinggame.js
@@ -0,0 +1,15 @@
+//generates a random numbeer between 1 and 100
+randomNumber = Math.floor(Math.random() * 101);
+
+var guessedNum = prompt('Please guess a number between 1 and 100!');
+var guessedNumInt = parseInt(guessedNum);
+
+if (guessedNumInt=== randomNumber){
+    console.log(`You guessed correctly in ${numOfGuesses} guesses!`);
+} else if (guessedNumInt < randomNumber) {
+    console.log('Higher!');
+} else {
+    console.log('Lower!');
+}
+
+
